58e1127a97fd0ce7d1dd6d4a73417aeafde34212
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-04-29 01:48:50 +0600

    flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import plugin from 'flowbite/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,5 +19,5 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, plugin],
 };
